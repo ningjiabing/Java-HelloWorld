bbdd76200e2f576e78e45d2ce0e8e5cae2c0d69e
jiabing.ning <jiabing.ning@yuwantech.com>
2021-01-13 18:42:11 +0800

    Iterator : 迭代器

--- src/com/java/demo/CollectionDemo.java
+++ src/com/java/demo/CollectionDemo.java
@@ -28,5 +28,34 @@ public class CollectionDemo {
             Integer value = map.get(key);
             System.out.println(key + " = " + value);
         }
+
+        // Queue
+        // 队列（Queue）是一种经常使用的集合。Queue实际上是实现了一个先进先出（FIFO：First In First Out）的有序表。它和List的区别在于，List可以在任意位置添加和删除元素，而Queue只有两个操作：
+        // 把元素添加到队列末尾；
+        // 从队列头部取出元素。
+
+        // int size()：获取队列长度；
+        // boolean add(E)/boolean offer(E)：添加元素到队尾；
+        // E remove()/E poll()：获取队首元素并从队列中删除；
+        // E element()/E peek()：获取队首元素但并不从队列中删除。
+        Queue<String> q = new LinkedList<>();
+        q.offer("apple");
+        System.out.println(q.poll()); // apple
+
+        // Stack
+        // 栈（Stack）是一种后进先出（LIFO：Last In First Out）的数据结构。
+        // Stack只有入栈和出栈的操作：
+        // 把元素压栈：push(E)；
+        // 把栈顶的元素“弹出”：pop(E)；
+        // 取栈顶元素但不弹出：peek(E)
+
+        // Iterator : 迭代器
+        // 使用迭代器的好处在于，调用方总是以统一的方式遍历各种集合类型，而不必关系它们内部的存储结构。
+        Iterator<String> it = list.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+
     }
 }
