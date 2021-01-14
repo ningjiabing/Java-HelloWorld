6cdbd90ceb1c19eec814af29edd3810cbb7cf102
jiabing.ning <jiabing.ning@yuwantech.com>
2021-01-14 18:59:47 +0800

    Date : 时间

---
+++ src/com/java/demo/DateDemo.java
@@ -0,0 +1,14 @@
+package com.java.demo;
+
+import java.util.Date;
+
+public class DateDemo {
+
+    public static void main(String[] args){
+        //获取当前时间:
+        Date date = new Date();
+
+        System.out.println(date.toLocaleString());
+    }
+
+}
