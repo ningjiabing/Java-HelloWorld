0bd75c50f2c8abd095b87ef84ce74855a027571d
jiabing.ning <357833631@qq.com>
2021-01-18 18:10:18 +0800

    Spring

---
+++ src/com/java/demo/SpringDemo.java
@@ -0,0 +1,37 @@
+package com.java.demo;
+
+public class SpringDemo {
+    // Spring
+    // Spring是一个支持快速开发Java EE应用程序的框架。它提供了一系列底层容器和基础设施，并可以和大量常用的开源框架无缝集成
+    // Spring最早是由Rod Johnson这哥们在他的《Expert One-on-One J2EE Development without EJB》一书中提出的用来取代EJB的轻量级框架。随后这哥们又开始专心开发这个基础框架，并起名为Spring Framework。
+    // 随着Spring越来越受欢迎，在Spring Framework基础上，又诞生了Spring Boot、Spring Cloud、Spring Data、Spring Security等一系列基于Spring Framework的项目。本章我们只介绍Spring Framework，即最核心的Spring框架。后续章节我们还会涉及Spring Boot、Spring Cloud等其他框架。
+
+    // Spring Framework
+    /*
+          Spring Framework主要包括几个模块：
+          支持IoC和AOP的容器；
+          支持JDBC和ORM的数据访问模块；
+          支持声明式事务的模块；
+          支持基于Servlet的MVC开发；
+          支持基于Reactive的Web开发；
+          以及集成JMS、JavaMail、JMX、缓存等其他模块。
+     */
+
+    // 容器
+    // 容器是一种为某种特定组件运行的运行提供必要支持的一个软件环境, 列入 Tomcat 就是一个servlet容器,他可以为servlet的运行提供运行环境
+
+    /*
+        在AOP编程中，我们经常会遇到下面的概念：
+        Aspect：切面，即一个横跨多个核心逻辑的功能，或者称之为系统关注点；
+        Joinpoint：连接点，即定义在应用程序流程的何处插入切面的执行；
+        Pointcut：切入点，即一组连接点的集合；
+        Advice：增强，指特定连接点上执行的动作；
+        Introduction：引介，指为一个已有的Java对象动态地增加新的接口；
+        Weaving：织入，指将切面整合到程序的执行流程中；
+        Interceptor：拦截器，是一种实现增强的方式；
+        Target Object：目标对象，即真正执行业务的核心逻辑对象；
+        AOP Proxy：AOP代理，是客户端持有的增强后的对象引用。
+     */
+
+
+}
